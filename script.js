54abbf99f4a83c225d786501e0906532b7047f6c
Andriy Yelisieienkov <eliseenkovandrej@gmail.com>
2020-05-15 16:23:23 +0300

    add

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 
 var toggler = document.querySelector(".header-nav__toggler");
 var navbar = document.querySelector(".header-nav");
+var lastScroll;
 
 toggler.onclick = function () {
   navbar.classList.toggle("show");
 };
+
+document.addEventListener("scroll", scrollHide);
+document.addEventListener("resize", scrollHide);
+
+function scrollHide() {
+  var secondScroll = window.pageYOffset;
+
+  if (!navbar.classList.contains('show')) {
+    if (lastScroll < secondScroll) {
+      navbar.classList.add("hide");
+    } else {
+      navbar.classList.remove("hide");
+    }
+  }
+
+  lastScroll = secondScroll;
+}
